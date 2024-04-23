9341b1621d5ed1101044ad5ceb49f00174f8da5d
mbc2579 <mbc2579@naver.com>
2024-04-23 19:23:49 +0900

    init

---
+++ src/main/java/com/example/fine_dust/FineDustApplication.java
@@ -0,0 +1,13 @@
+package com.example.fine_dust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FineDustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FineDustApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fine_dust/FineDustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fine_dust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FineDustApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
